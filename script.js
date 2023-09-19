8584c37910ffdd928dabd5ef037a5e4c26f0ed39
Mk.2 <55669745+mk2-mc@users.noreply.github.com>
2023-09-19 22:39:51 +0100

    Update

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 const form = document.querySelector(".form");
 const input = document.querySelectorAll(".input");
+const errorText = document.querySelectorAll(".error-text");
 
 function validate() {
   let valid = true;
   
-  // Input empty
   for (let i = 0; i < input.length; i++) {
+    const name = input[i].getAttribute("name");
+    
+    // Input empty
     if (input[i].value === "") {
       input[i].classList.add("error");
+      errorText[i].classList.remove("hidden");
+      errorText[i].innerHTML = `${name} cannot be empty`;
       valid = false;
+    } else {
+      
+      // Email invalid
+      const emailText = document.querySelector(".email-text");
+      const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      
+      if ((name === "Email")
+      && (!emailFormat.test(input[i].value))) {
+        input[i].classList.add("error");
+        emailText.classList.remove("hidden");
+        emailText.innerHTML = "Looks like this is not an email";
+        valid = false;        
+      }
+      
+      // Password invalid
+      const passwordText = document.querySelector(".password-text");
+      const length = input[i].value.length;
+      
+      if ((name === "Password")
+      && ((length < 8) || (length > 24))) {
+        input[i].classList.add("error");
+        passwordText.classList.remove("hidden");
+        passwordText.innerHTML = "Password must be between 8 and 24 characters";
+        valid = false;        
+      }
     }
-  }
-  
-  // Email invalid
-  const email = document.querySelector("#email");
-  const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  
-  if (!emailFormat.test(email.value)) {
-    email.classList.add("error");
-    valid = false;
-  }
-  
-  // Password invalid
-  const password = document.querySelector("#password");
-  const length = password.value.length;
-  
-  if ((length < 8) || (length > 24)) {
-    password.classList.add("error");
-    valid = false;
   }
   
   return valid;
@@ -37,6 +49,7 @@ function validate() {
 for (let i = 0; i < input.length; i++) {
   input[i].addEventListener("input", () => {
     input[i].classList.remove("error");
+    errorText[i].classList.add("hidden");
   });
 }
 
